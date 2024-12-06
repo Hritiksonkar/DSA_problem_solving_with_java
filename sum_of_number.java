7e3dd1e7bdbefff4bbf269f871a3e9971e4becff
Hritiksonkar <147808870+Hritiksonkar@users.noreply.github.com>
2024-12-06 22:56:23 +0530

    05/11/24 simple program of add two number

---
+++ sum_of_number.java
@@ -0,0 +1,10 @@
+public class sum_of_number {
+    public static void main(String[] args) {
+        System.out.println(sum(10, 20));
+        
+    }
+    static int sum(int a,int b){
+        int num= a+b;
+        return num;
+    }
+}
